b8d84703572b1a23d5bb2da01c273588c32e1826
Andy R. Terrel <andy.terrel@gmail.com>
2015-02-15 21:40:42 -0600

    Use // instead of http:// in SITE_URL to avoid issues with mixed content when hitting the site with https://

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://conference.scipy.org'
+SITEURL = '//conference.scipy.org'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
